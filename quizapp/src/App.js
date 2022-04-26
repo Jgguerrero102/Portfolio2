803798abe979c122841708f4fdf817993b7db297
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-26 06:17:52 -0400

    commt

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -32,6 +32,56 @@ let questions = [
   },
 ]
 
+export default function App() {
+  let { score, setScore } = useState()
+  let [answers, setAnswers] = useState([])
+  let checkAnswers = useCallback((data, qAnswers) => {
+    let answersCorrect = true
+    for (let i = 0; i < data.answers.length; i++) {
+      let qCorrect
+      if (data.answers[i].correct) {
+        qCorrect = qAnswers.includes(i)
+      } else {
+        qCorrect = !qAnswers.includes(i)
+      }
+      answersCorrect = answersCorrect && qCorrect
+    }
+    if (answersCorrect) {
+      setScore(prevScore => {
+        if (prevScore !== undefined) {
+       return prevScore + 1
+      } else {
+        return 1
+      }
+      })
+    } else {
+      setScore(prevScore => prevScore === undefined ? 0 : prevScore)
+    }
+  }, [answers, score])
+  return <>
+    <View style={styles.container}>
+      <Text>Quiz Application</Text>
+      <FlatList data={questions} renderItem={({item, index}) => 
+        <Question showAnswers={score !== undefined} data={item} key={index}
+          setAnswers={
+            (answers) => {
+            setAnswers(prev => {
+              prev[index] = answers
+              return [...prev]
+            })
+          }}
+      answer={answers[index]}
+      ></Question>
+      }></FlatList>
+    <Button title="submit" onPress={
+      () => questions.forEach((q,i) => checkAnswers(q, answers[i]))} 
+      disabled={answers.length == 0} Submit></Button>
+    {score !== undefined ? <Text>Score: {score} </Text> : undefined}
+    <StatusBar style="auto" />
+  </View>
+  </>
+}
+
 
 const styles = StyleSheet.create({
   button: {
