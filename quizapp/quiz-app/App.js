1576cee7a25f54f4ceb77ac1841dff2e8851c427
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-26 06:09:53 -0400

    Update App.js

--- quizapp/quiz-app/App.js
+++ quizapp/quiz-app/App.js
@@ -6,38 +6,69 @@ import { Card, CheckBox } from 'react-native-elements';
 
 let questions = [
   {
-    title: "Question Title",
+    title: "How many moons does the Earth have?",
     multipleAnswers: true,
     answers: [
-      { correct: false, title: "Question 1" },
-      { correct: true, title: "Question 2" },
-      { correct: true, title: "Question 3" },
-      { correct: true, title: "Question 4" }
+      { correct: true, title: "1" },
+      { correct: false, title: "2" },
+      { correct: false, title: " 3" },
+      { correct: false, title: "4" }
     ]
   },
   {
-    title: "Question Title 2",
+    title: "True or False: Gravity has less of an effect in space",
     multipleAnswers: true,
     answers: [
-      { correct: false, title: "Question A" },
-      { correct: true, title: "Question B" },
-      { correct: false, title: "Question C" },
-      { correct: false, title: "Question D" }
+      { correct: false, title: "False" },
+      { correct: true, title: "True" },
+    ]
+  },
+  {
+  title: "Mass x Gravity = ",
+    multipleAnswers: true,
+    answers: [
+      { correct: true, title: "Weight" },
     ]
   },
 ]
 
+
 const styles = StyleSheet.create({
-  incorrect: {
-    textDecorationLine: "line-through",
-    textDecorationStyle: "solid"
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+},
+error: {
+    color: "red",
+    paddingVertical: 8
+},
 
+welcome: {
+    fontSize: 16,
+    paddingVertical: 15,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+},
+text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+},
+
+textinput: {
+    fontSize: 20,
+    width: 200,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairWidth,
+    padding: 5,
+    margin: 5
+},
+});
 
