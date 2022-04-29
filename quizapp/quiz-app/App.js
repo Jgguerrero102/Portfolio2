3c7dcab4fcb95f1fd636d885c1adf836b6936692
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-29 10:30:36 -0400

    Update App.js

--- quizapp/quiz-app/App.js
+++ quizapp/quiz-app/App.js
@@ -3,83 +3,67 @@ import { useCallback, useState } from 'react';
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { Card, CheckBox } from 'react-native-elements';
 
-
-let questions = [
-  {
-    title: "How many moons does the Earth have?",
-    multipleAnswers: true,
-    answers: [
-      { correct: true, title: "1" },
-      { correct: false, title: "2" },
-      { correct: false, title: " 3" },
-      { correct: false, title: "4" }
-    ]
-  },
-  {
-    title: "True or False: Gravity has less of an effect in space",
-    multipleAnswers: true,
-    answers: [
-      { correct: false, title: "False" },
-      { correct: true, title: "True" },
-    ]
-  },
-  {
-  title: "Mass x Gravity = ",
-    multipleAnswers: true,
-    answers: [
-      { correct: true, title: "Weight" },
-    ]
-  },
-]
-
 export default function App() {
-  let { score, setScore } = useState()
-  let [answers, setAnswers] = useState([])
-  let checkAnswers = useCallback((data, qAnswers) => {
-    let answersCorrect = true
-    for (let i = 0; i < data.answers.length; i++) {
-      let qCorrect
-      if (data.answers[i].correct) {
-        qCorrect = qAnswers.includes(i)
-      } else {
-        qCorrect = !qAnswers.includes(i)
-      }
-      answersCorrect = answersCorrect && qCorrect
-    }
-    if (answersCorrect) {
-      setScore(prevScore => {
-        if (prevScore !== undefined) {
-       return prevScore + 1
-      } else {
-        return 1
-      }
-      })
-    } else {
-      setScore(prevScore => prevScore === undefined ? 0 : prevScore)
-    }
-  }, [answers, score])
-  return <>
-    <View style={styles.container}>
-      <Text>Quiz Application</Text>
-      <FlatList data={questions} renderItem={({item, index}) => 
-        <Question showAnswers={score !== undefined} data={item} key={index}
-          setAnswers={
-            (answers) => {
-            setAnswers(prev => {
-              prev[index] = answers
-              return [...prev]
-            })
-          }}
-      answer={answers[index]}
-      ></Question>
-      }></FlatList>
-    <Button title="submit" onPress={
-      () => questions.forEach((q,i) => checkAnswers(q, answers[i]))} 
-      disabled={answers.length == 0} Submit></Button>
-    {score !== undefined ? <Text>Score: {score} </Text> : undefined}
-    <StatusBar style="auto" />
-  </View>
+const questions = [
+  {
+    questionText: "How many moons does the Earth have?",
+    multipleAnswers: true,
+    answerOptions: [
+      { correct: true, answerText: "1" },
+      { correct: false, answerText: "2" },
+      { correct: false, answerText: " 3" },
+      { correct: false, answerText: "4" }
+    ]
+  },
+  {
+    questionText: "True or False: Gravity has less of an effect in space",
+    multipleAnswers: true,
+    answerOptions: [
+      { correct: false, answerText: "False" },
+      { correct: true, answerText: "True" },
+    ]
+  },
+  {
+    questionText: "Mass x Gravity = ",
+    multipleAnswers: true,
+    answersOptions: [
+      { correct: true, answerText: "Weight" },
+    ]
+  },
+];
+
+const [currentQuestion, setCurrentQuestion] = useState();
+
+const handleAnswerButtonClick = () => {
+const nextQuestion = currentQuestion + 1;
+if(nextQuestion < questions.length) {
+setCurrentQuestion(nextQuestion);
+} else {
+  alert("You have completed the test!")
+}
+}
+
+return (
+ <div className='app'>
+ { false ? (
+   <div className='score'> You scored 1 out of {questions.length}</div>
+ ) : (
+  <>
+     <div className='questions'>
+     <div className='questions-count'>
+        <span>Question 1</span>/{questions.length}
+     </div>
+     <div className='question-text'>{questions[currentQuestion].questionText}</div>
+     </div>
+     <div className='answer-section'>
+      {questions[currentQuestion].answerOption.map((answerOption) => (
+          <button onClick={handleAnswerButtonClick}>{answerOption.answerText}</button>
+      ))}
+     </div>
   </>
+ )}
+ </div>
+);
 }
 
 
@@ -91,7 +75,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'black',
+    backgroundColor: 'aqua',
 },
 error: {
     color: "red",
