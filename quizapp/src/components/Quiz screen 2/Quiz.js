23d2b4abbc22431bc1fddbcd1457b682684e63e2
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-29 23:43:04 -0400

    Update Quiz.js

--- quizapp/src/components/Quiz screen 2/Quiz.js
+++ quizapp/src/components/Quiz screen 2/Quiz.js
@@ -7,10 +7,10 @@ function Quiz() {
     return <div className="Quiz">
         <h1>{Quiz[currentQuestion].prompt}</h1>
         <div className="options">
-            <button>{Questions[currentQuestion].optionA}</button>
-            <button>{Questions[currentQuestion].optionB}</button>
-            <button>{Questions[currentQuestion].optionC}</button>
-            <button>{Questions[currentQuestion].optionD}</button>
+            <button onClick={() => setOptionChosen("A")}>{Questions[currentQuestion].optionA}</button>
+            <button onClick={() => setOptionChosen("B")}>{Questions[currentQuestion].optionB}</button>
+            <button onClick={() => setOptionChosen("C")}>{Questions[currentQuestion].optionC}</button>
+            <button onClick={() => setOptionChosen("D")}>{Questions[currentQuestion].optionD}</button>
         </div>
 
         <button> Next Question</button>
